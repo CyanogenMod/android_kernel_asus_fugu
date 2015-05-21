90de589f61a28aad0f08053fe8615cae51cadd16
Tom Marshall <tdm@cyngn.com>
2015-05-21 15:44:52 -0700

    fs: ext4: Support transparent compression
    Change-Id: I78499d8de8815e596b91c1b529aba9c965640925

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -30,6 +30,7 @@
 #include <linux/blockgroup_lock.h>
 #include <linux/percpu_counter.h>
 #include <crypto/hash.h>
+#include <linux/fs-xcomp.h>
 #ifdef __KERNEL__
 #include <linux/compat.h>
 #endif
@@ -921,6 +922,10 @@ struct ext4_inode_info {
 
 	/* Precomputed uuid+inum+igen checksum for seeding inode checksums */
 	__u32 i_csum_seed;
+
+#ifdef CONFIG_FS_TRANSPARENT_COMPRESSION
+	struct xcomp_inode_info i_xcomp_info;
+#endif
 };
 
 /*
